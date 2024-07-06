26ab55c5383d5f7863f22f894fb103d6bbb87b4a
Makiesse Morais <makiesse.dev@gmail.com>
2024-07-06 16:25:37 +0100

    feat: strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int	s1_len;
+	int	s2_len;
+	int	i;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = malloc((s1_len + s2_len) * sizeof(char) + 1);
+	i = 0;
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, s1_len + 1);
+	while (s2[i])
+	{
+		str[i + s1_len] = s2[i];
+		i++;
+	}
+	str[s1_len + s2_len] = '\0';
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -25,4 +25,5 @@ int		atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 #endif
