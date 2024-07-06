abde3ea789963739a18af6b62c23c369afe144d1
Makiesse <makiesse.dev@gmail.com>
2024-07-06 09:10:50 +0100

    feat ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < (size - 1) && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (sizeof(src));
+}
