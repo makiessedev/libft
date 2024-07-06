2f9fb1b7d32853edd341cdf18ee681460badd851
Makiesse <makiesse.dev@gmail.com>
2024-07-06 09:13:43 +0100

    feat implements

--- libft.h
+++ libft.h
@@ -1,7 +1,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <sys/types.h>
+# include <stddef.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -14,4 +14,9 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*memmove(void *dest, const void *src, size_t n);
 
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 #endif
