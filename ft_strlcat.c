57fdafcd9634d0d542ad059d541c413090eda8b7
Makiesse <makiesse.dev@gmail.com>
2024-07-06 09:10:31 +0100

    feat ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (size <= dest_len)
+		return (dest_len + size);
+	while (dest_len + i + 1 < size && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
