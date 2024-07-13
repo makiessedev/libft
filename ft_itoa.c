aee1a9cf4220c71dbbbbd17c50f591a4fc651dd2
Makiesse Morais <makiesse.dev@gmail.com>
2024-07-13 07:56:59 +0100

    feat verify malloc failed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mmorais <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 07:51:12 by mmorais           #+#    #+#             */
-/*   Updated: 2024/07/13 07:52:14 by mmorais          ###   ########.fr       */
+/*   Updated: 2024/07/13 07:55:29 by mmorais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -35,6 +35,8 @@ char	*ft_itoa(int n)
 	digit_numbers = ft_digit_numbers(n);
 	num = n;
 	str_n = malloc((digit_numbers + 1) * sizeof(char));
+	if (!str_n)
+		return (NULL);
 	if (n < 0)
 	{
 		num *= -1;
