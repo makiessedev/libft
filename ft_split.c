fb77b3694ab21a5d051d890e259e01e9ae2657a9
Makiesse Morais <makiesse.dev@gmail.com>
2024-07-13 04:24:35 +0100

    fix nomminette errors

--- ft_split.c
+++ ft_split.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmorais <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/13 04:17:22 by mmorais           #+#    #+#             */
+/*   Updated: 2024/07/13 04:23:53 by mmorais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
 static int	ft_strings_num(char const *s, char c)
@@ -20,7 +31,6 @@ static int	ft_strings_num(char const *s, char c)
 			i++;
 	}
 	return (counter + 1);
-
 }
 
 static int	ft_word_size(const char *str, char c, int i)
@@ -41,27 +51,24 @@ static void	ft_free(char **strs, int i)
 	while (i-- > 0)
 		free(strs[i]);
 	free(strs);
-
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int 		i;
+	int		i;
 	int		j;
 	int		word_size;
-	int		strs_num;
 	char	**newstr;
 
 	i = 0;
-	j = 0;
-	strs_num = ft_strings_num(s, c);
-	newstr = (char **)malloc(strs_num * sizeof(char *) + 2);
+	j = -1;
+	newstr = (char **)malloc(ft_strings_num(s, c) * sizeof(char *) + 2);
 	if (!newstr)
 		return (NULL);
-	while (s[i] && strs_num > 0)
+	while (++j < ft_strings_num(s, c))
 	{
 		while (s[i] == c)
-			i++;	
+			i++;
 		word_size = ft_word_size(s, c, i);
 		newstr[j] = ft_substr(s, i, word_size);
 		if (!newstr[j])
@@ -70,8 +77,6 @@ char	**ft_split(char const *s, char c)
 			return (NULL);
 		}
 		i += word_size;
-		strs_num--;
-		j++;
 	}
 	newstr[j] = '\0';
 	return (newstr);
