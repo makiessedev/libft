6f89565206a21efaef5ee6ff3e604c03aeefca95
Makiesse <makiesse.dev@gmail.com>
2024-07-19 14:32:22 +0100

    feat ft_strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -41,4 +41,5 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 #endif
